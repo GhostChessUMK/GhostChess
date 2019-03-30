7385b87ba757a2d86f9296a70d825c1e168b05ae
Marcin Burak <marcin.burak@outlook.com>
2019-03-30 21:31:38 +0100

    We done?

--- src/GhostChess.Board/Program.cs
+++ src/GhostChess.Board/Program.cs
@@ -1,18 +1,15 @@
-﻿using RJCP.IO.Ports;
-using System;
-using System.Threading;
-using System.Linq;
-using System.Text;
-using GhostChess.RaspberryPi;
-using System.Collections;
-using System.Collections.Generic;
-using GhostChess.Board.Abstractions;
+﻿using GhostChess.Board.Abstractions;
 using GhostChess.Board.Abstractions.Configuration;
-using GhostChess.Board.Abstractions.Models;
 using GhostChess.Board.Abstractions.Helpers;
-using static GhostChess.Board.Abstractions.Constants;
+using GhostChess.Board.Abstractions.Models;
+using GhostChess.RaspberryPi;
 using Microsoft.AspNetCore.SignalR.Client;
+using RJCP.IO.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using static GhostChess.Board.Abstractions.Constants;
 
 namespace GhostChess.Board
 {
@@ -22,7 +19,7 @@ namespace GhostChess.Board
 
         public static async Task Main(string[] args)
         {
-            if(args.Count().Equals(0))
+            if (args.Count().Equals(0))
             {
                 Console.WriteLine("Error! Put serial port name in arguments ex.");
                 Console.WriteLine("sudo dotnet GhostChess.Board.dll /dev/ttyUSB0");
@@ -43,17 +40,11 @@ namespace GhostChess.Board
             registerEdges.Register(nodes);
 
             Console.WriteLine("Configuring serial port...");
-            SerialPortStream serial = new SerialPortStream(SerialPortName, BaudRate);           
+            SerialPortStream serial = new SerialPortStream(SerialPortName, BaudRate);
 
             Console.WriteLine("Configuring gpio pins...");
             Gpio gpio = new Gpio(RaspberryPi.Enums.Pins.Gpio3);
             var controller = new Controller(gpio, serial);
-
-            Console.WriteLine("Initializing SingalR...");
-            var connection = new HubConnectionBuilder()
-              .WithUrl("https://localhost:5000/chess?Password=P@ssw0rd&Board=true")
-              .Build();
-            await connection.StartAsync();
 
             Console.WriteLine("Moving to zero...");
             serial.Open();
@@ -66,7 +57,23 @@ namespace GhostChess.Board
 
             var currentNode = nodes.First(t => t.Name.Equals("LI0"));
 
-            while (true)
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine();
+            Console.WriteLine($"Current node: {currentNode.Name}");
+            Console.WriteLine($"Current x: {currentNode.X}");
+            Console.WriteLine($"Current y: {currentNode.Y}");
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("Initializing SingalR...");
+
+            var connection = new HubConnectionBuilder()
+            .WithUrl("https://localhost:5000/chess?Password=P@ssw0rd&Board=true")
+            .Build();
+
+            connection.On<string, string>("Move", (source, destination) =>
             {
                 Console.WriteLine("-----------------------------------");
                 Console.WriteLine();
@@ -75,20 +82,10 @@ namespace GhostChess.Board
                 Console.WriteLine($"Current y: {currentNode.Y}");
                 Console.WriteLine();
 
-                Node sourceNode = null, destinationNode = null;
-                connection.On<string, string>("Move", (source, destination) =>
-                {
-                    sourceNode = nodes.First(x => x.Name.Equals(source));
-                    destinationNode = nodes.First(x => x.Name.Equals(destination));
-                });
-                //Console.Write("Put source: ");
-                //string source = Console.ReadLine();
-                //Console.Write("Put destination: ");
-                //string destination = Console.ReadLine();
-                Console.WriteLine();
+                var sourceNode = nodes.First(x => x.Name.Equals(source));
+                var destinationNode = nodes.First(x => x.Name.Equals(destination));
 
-                //var sourceNode = nodes.First(x => x.Name.Equals(source));
-                //var destinationNode = nodes.First(x => x.Name.Equals(destination));
+                Console.WriteLine();
 
                 if (destinationNode.isEmpty == false)
                 {
@@ -96,7 +93,7 @@ namespace GhostChess.Board
                     List<Node> colorNodes;
                     var color = Abstractions.Enums.Colors.Black;
 
-                    if(color == Abstractions.Enums.Colors.White)
+                    if (color == Abstractions.Enums.Colors.White)
                     {
                         colorNodes = NodeHelper.GetLeftCentralNodes(nodes);
                     }
@@ -105,9 +102,9 @@ namespace GhostChess.Board
                         colorNodes = NodeHelper.GetRightCentralNodes(nodes);
                     }
 
-                    foreach(var node in colorNodes)
+                    foreach (var node in colorNodes)
                     {
-                        if(node.isEmpty == false)
+                        if (node.isEmpty == false)
                         {
                             freeNode = node;
                             break;
@@ -119,10 +116,10 @@ namespace GhostChess.Board
                         .MagnetOn();
 
                     var path = pathfinder.FindPath(destinationNode, freeNode);
-                    for(int i = 1; i < path.Count; i++)
+                    for (int i = 1; i < path.Count; i++)
                     {
                         var vector = new Vector(path[i - 1], path[i]);
-                        if(vector.X != 0 && vector.Y != 0)
+                        if (vector.X != 0 && vector.Y != 0)
                         {
                             controller.Move(path[i - 1], path[i]).Sleep((int)(vector.Length * mmPerSec + AdditionalXYSleep));
                         }
@@ -135,19 +132,19 @@ namespace GhostChess.Board
                     currentNode = path.Last();
                 }
 
-                if(currentNode != sourceNode)
+                if (currentNode != sourceNode)
                 {
                     controller.Move(currentNode, sourceNode).Sleep((int)(Vector.GetLength(currentNode, sourceNode) * mmPerSec + AdditionalXYSleep));
                 }
 
-                if(sourceNode != destinationNode)
+                if (sourceNode != destinationNode)
                 {
                     controller.MagnetOn();
                     var path = pathfinder.FindPath(sourceNode, destinationNode);
-                    for(int i = 1; i < path.Count; i++)
+                    for (int i = 1; i < path.Count; i++)
                     {
                         var vector = new Vector(path[i - 1], path[i]);
-                        if(vector.X != 0 && vector.Y != 0)
+                        if (vector.X != 0 && vector.Y != 0)
                         {
                             controller.Move(path[i - 1], path[i]).Sleep((int)(vector.Length * mmPerSec + AdditionalXYSleep));
                         }
@@ -161,7 +158,10 @@ namespace GhostChess.Board
                 }
 
                 controller.Execute();
-            }
+            });
+
+            await connection.StartAsync();
+            Console.WriteLine("SignalR initialized.");
         }
     }
 }
